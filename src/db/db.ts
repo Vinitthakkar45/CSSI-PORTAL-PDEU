1c7e9c41eb54f0e63eb1af33537f67678653ba47
Vinitthakkar45 <45vinitthakkar@gmail.com>
2025-02-23 18:18:12 +0530

    chore: initial drizzle setup

---
+++ src/db/db.ts
@@ -0,0 +1,6 @@
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+
+console.log(process.env.DATABASE_URL)
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle({ client: sql });
