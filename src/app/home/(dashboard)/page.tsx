544e12e6a744fe03056141719987ae2fbff6af7a
Vinitthakkar45 <45vinitthakkar@gmail.com>
2025-03-19 12:41:19 +0530

    chore(template): student,faculty,admin components

--- src/app/home/(dashboard)/page.tsx
+++ src/app/home/(dashboard)/page.tsx
@@ -7,35 +7,62 @@ import StatisticsChart from '@/components/Home/ecommerce/StatisticsChart';
 import RecentOrders from '@/components/Home/ecommerce/RecentOrders';
 import DemographicCard from '@/components/Home/ecommerce/DemographicCard';
 
+import AdminDashboard from '@/components/Admin/Dashboard';
+import StudentDashboard from '@/components/Student/Dashboard';
+import FacultyDashboard from '@/components/Faculty/Dashboard';
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+
 export const metadata: Metadata = {
   title: 'Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template',
   description: 'This is Next.js Home for TailAdmin Dashboard Template',
 };
 
-export default function Ecommerce() {
+export default async function Dashboard() {
+  const session = await getServerSession(authOptions);
+  const userRole = session?.user?.role;
+
+  // Render based on role
+  let dashboardContent;
+
+  switch (userRole) {
+    case 'admin':
+      dashboardContent = <AdminDashboard />;
+      break;
+    case 'faculty':
+      dashboardContent = <FacultyDashboard />;
+      break;
+    case 'student':
+      dashboardContent = <StudentDashboard />;
+      break;
+  }
+
   return (
-    <div className="grid grid-cols-12 gap-4 md:gap-6">
-      <div className="col-span-12 space-y-6 xl:col-span-7">
-        <EcommerceMetrics />
+    <>
+      <div className="m-10">{dashboardContent}</div>
+      <div className="grid grid-cols-12 gap-4 md:gap-6">
+        <div className="col-span-12 space-y-6 xl:col-span-7">
+          <EcommerceMetrics />
 
-        <MonthlySalesChart />
-      </div>
+          <MonthlySalesChart />
+        </div>
 
-      <div className="col-span-12 xl:col-span-5">
-        <MonthlyTarget />
-      </div>
+        <div className="col-span-12 xl:col-span-5">
+          <MonthlyTarget />
+        </div>
 
-      <div className="col-span-12">
-        <StatisticsChart />
-      </div>
+        <div className="col-span-12">
+          <StatisticsChart />
+        </div>
 
-      <div className="col-span-12 xl:col-span-5">
-        <DemographicCard />
-      </div>
+        <div className="col-span-12 xl:col-span-5">
+          <DemographicCard />
+        </div>
 
-      <div className="col-span-12 xl:col-span-7">
-        <RecentOrders />
+        <div className="col-span-12 xl:col-span-7">
+          <RecentOrders />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
---
+++ src/components/Admin/Dashboard.tsx
@@ -0,0 +1,26 @@
+'use client';
+import { useSession } from 'next-auth/react';
+
+const Dashboard = () => {
+  const { data: session, status } = useSession();
+
+  if (status === 'loading') {
+    return <p>Loading...</p>;
+  }
+
+  if (status === 'unauthenticated') {
+    return <p>Please sign in.</p>;
+  }
+
+  return (
+    <div className="text-amber-100">
+      <p>ADMIN DASHBOARD</p>
+      <p>Welcome, {session?.user?.role}!</p>
+      <p>Welcome, {session?.user?.email}!</p>
+      <p>Welcome, {session?.user?.name}!</p>
+      <p>Welcome, {session?.user?.id}!</p>
+    </div>
+  );
+};
+
+export default Dashboard;
