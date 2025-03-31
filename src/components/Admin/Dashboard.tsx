80990a849a48d83834fa5789b199b4c24a10c1fe
kaushikjaincodes <kaushikjain2626@gmail.com>
2025-04-01 00:53:24 +0530

    feat(cards): add admin stage unlock privileges

--- src/components/Admin/Dashboard.tsx
+++ src/components/Admin/Dashboard.tsx
@@ -1,6 +1,14 @@
 'use client';
+import { useState } from 'react';
 import { useSession } from 'next-auth/react';
 import { redirect } from 'next/navigation';
+import StageProgress from './StageProgress';
+import StageCard from './StageCard';
+import { stages, Stage } from './utils/stages';
+import Button from '../Home/ui/button/Button';
+import { InfoModal } from './AdminModals';
+
+type StageStatus = 'locked' | 'current' | 'completed';
 
 const Dashboard = () => {
   const { data: session, status } = useSession({
@@ -9,6 +17,9 @@ const Dashboard = () => {
       redirect('/signin');
     },
   });
+
+  const [currentStage, setCurrentStage] = useState<number>(1);
+  const [showModal, setShowModal] = useState(false);
 
   const handleAssignMentors = async () => {
     try {
@@ -30,17 +41,53 @@ const Dashboard = () => {
     return <p>Loading...</p>;
   }
 
+  const handleUnlockStage = () => {
+    setShowModal(true);
+    console.log('open');
+  };
+
+  const handleModalClose = () => {
+    setShowModal(false);
+    if (currentStage < 4) {
+      setCurrentStage((prev) => prev + 1);
+    }
+  };
+  const handleModalCross = () => {
+    setShowModal(false);
+  };
+
+  const getStageStatus = (stageNumber: number): StageStatus => {
+    if (stageNumber < currentStage) return 'completed';
+    if (stageNumber === currentStage) return 'current';
+    return 'locked';
+  };
+
   return (
     <>
-      <button
-        className="m-5 inline-flex items-center gap-2 rounded-lg border border-gray-300 bg-white px-4 py-2.5 text-theme-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200"
-        onClick={() => handleAssignMentors()}
-      >
-        Assign Mentor
-      </button>
-      <button className="m-5 inline-flex items-center gap-2 rounded-lg border border-gray-300 bg-white px-4 py-2.5 text-theme-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200">
-        Assign Faculty
-      </button>
+      {showModal && <InfoModal isOpen={showModal} onCloseCross={handleModalCross} onClose={handleModalClose} />}
+      <div className="container pb-4 mx-auto">
+        <StageProgress currentStage={currentStage} totalStages={stages.length} handleButtonClick={handleUnlockStage} />
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
+          {stages.map((stage) => (
+            <StageCard
+              key={stage.number}
+              number={stage.number}
+              title={stage.title}
+              description={stage.description}
+              status={getStageStatus(stage.number)}
+            />
+          ))}
+        </div>
+      </div>
+      <div>
+        <Button size="md" variant="primary" className="mr-4" onClick={handleAssignMentors}>
+          Assign Mentor
+        </Button>
+        <Button size="md" variant="primary">
+          Assign Faculty
+        </Button>
+      </div>
     </>
   );
 };
