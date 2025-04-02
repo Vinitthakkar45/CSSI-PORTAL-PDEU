d367074a6d158786ee097f0d3aa8c81f3528af09
kaushikjaincodes <kaushikjain2626@gmail.com>
2025-04-02 10:46:01 +0530

    feat(api) : stage fetch and change api

---
+++ src/app/api/stage/route.ts
@@ -0,0 +1,32 @@
+import { db } from '@/drizzle/db';
+import { stage } from '@/drizzle/schema';
+import { NextRequest, NextResponse } from 'next/server';
+import { eq } from 'drizzle-orm';
+
+export async function GET() {
+  try {
+    const date = new Date();
+    const year = date.getFullYear();
+    const currentStage = await db.select({ stage: stage.stage }).from(stage).where(eq(stage.year, year));
+    return NextResponse.json({ stage: currentStage }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 404 });
+  }
+}
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  try {
+    const date = new Date();
+    const year = date.getFullYear();
+    const { changedStage } = await req.json();
+
+    const response = await db.update(stage).set({ stage: changedStage }).where(eq(stage.year, year));
+    if (response.rowCount > 0) {
+      return NextResponse.json({ message: 'Update successful', affectedRows: response.rowCount }, { status: 200 });
+    } else {
+      return NextResponse.json({ message: 'No rows updated. Record not found.' }, { status: 404 });
+    }
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
+}
