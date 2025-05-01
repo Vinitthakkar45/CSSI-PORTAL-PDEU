300e3622cde22542220c5b78611cdb48f572f1bb
kaushikjaincodes <kaushikjain2626@gmail.com>
2025-05-01 21:38:28 +0530

    fix: profile image in admin dashboard

--- src/components/Admin/CoordinatorTable.tsx
+++ src/components/Admin/CoordinatorTable.tsx
@@ -98,7 +98,7 @@ const CoordinatorTable = () => {
       <div className="overflow-hidden rounded-2xl border border-gray-200 bg-white px-4 pb-3 pt-4 dark:border-gray-800 dark:bg-white/[0.03] sm:px-6">
         <div className="flex flex-col gap-2 mb-4 sm:flex-row sm:items-center sm:justify-between">
           <div>
-            <h3 className="text-lg font-semibold text-gray-800 dark:text-white/90">Faculty Details</h3>
+            <h3 className="text-lg font-semibold text-gray-800 dark:text-white/90">Coordinator Details</h3>
           </div>
           <div className="flex flex-col sm:flex-row items-start sm:items-center gap-3">
             <div className="w-full sm:w-auto mb-2 sm:mb-0">
